ef0673cadb95aef8425f2defd0cfcbc4cf97ba64
Nicolas <nicolas.programador@gmail.com>
2025-05-21 18:29:28 -0400

    Microservicio curso listo

--- microservice-student/src/main/java/com/microservice/student/controller/StudentController.java
+++ microservice-student/src/main/java/com/microservice/student/controller/StudentController.java
@@ -2,6 +2,7 @@ package com.microservice.student.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -10,6 +11,10 @@ import org.springframework.web.bind.annotation.RestController;
 import com.microservice.student.service.IStudentService;
 import com.microservice.student.model.Student;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 
 @RestController
@@ -25,6 +30,17 @@ public class StudentController {
     @ResponseStatus(HttpStatus.CREATED)
     public void saveStudent(@RequestBody Student student){
         iStudentService.save(student);
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<?> findAllStudents(){
+        return ResponseEntity.ok(iStudentService.findAll());
+    }
+    
+
+    @GetMapping("/search/{id}")    
+    public ResponseEntity<?> findById(@PathVariable Long id){
+        return ResponseEntity.ok(iStudentService.findById(id));
     }
 
 }
