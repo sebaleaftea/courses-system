753b9a47a31d564bcf2b56cbdc2dade7befc9fe2
Nicolas <nicolas.programador@gmail.com>
2025-05-21 19:19:34 -0400

    Bug Fixed

--- microservice-course/src/main/java/com/microservice/course/controller/CourseController.java
+++ microservice-course/src/main/java/com/microservice/course/controller/CourseController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import com.microservice.course.model.Course;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
+
 
 
 
--- microservice-student/src/main/java/com/microservice/student/controller/StudentController.java
+++ microservice-student/src/main/java/com/microservice/student/controller/StudentController.java
@@ -13,7 +13,7 @@ import com.microservice.student.model.Student;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
+
 
 
 
